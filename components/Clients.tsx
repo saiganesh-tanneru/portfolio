b7ebea96257cc73cc33cd00e1d54a3a5c30fa296
saiganesh-tanneru <ganesh.sai42@gmail.com>
2024-10-12 22:18:34 -0500

    added approach, experience, clients and footer

---
+++ components/Clients.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
+import { testimonials } from '@/data'
+
+const Clients = () => {
+    return (
+        <div className="py-20" id="testimonials">
+            <h1 className="heading">
+                Kind words from {" "}
+                <span className="text-purple">satisfied clients</span>
+            </h1>
+            <div className="flex flex-col items-center">
+                <div
+                    className='h-[50vh] md:h-[30rem] rounded-md flex flex-col
+                        antialiased items-center relative overflow-hidden max-lg:mt-10'
+                >
+                    <InfiniteMovingCards
+                        items={testimonials}
+                        direction='right'
+                        speed='slow'
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Clients
--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,22 +7,22 @@ import { PinContainer } from "./ui/3d-pin";
 
 const RecentProjects = () => {
     return (
-        <div className="py-20">
+        <div className="py-20" id="projects">
             <h1 className="heading">
                 A small selection of{" "}
                 <span className="text-purple">recent projects</span>
             </h1>
-            <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+            <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
                 {projects.map((item) => (
                     <div
-                        className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+                        className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center w-[80vw] sm:w-[570px]"
                         key={item.id}
                     >
                         <PinContainer
                             title={"visit"}
                             href={"http://www.google.com"}
                         >
-                            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+                            <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden h-[30vh] lg:h-[30vh] mb-10 sm:h-[40vh]">
                                 <div
                                     className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                                     style={{ backgroundColor: "#13162D" }}
