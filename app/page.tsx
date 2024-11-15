ff6c2ee75a96cd69f12596f81a14a03bbe7a5f57
saiganesh-tanneru <ganesh.sai42@gmail.com>
2024-11-14 19:44:24 -0600

    fixed lint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Approach from "@/components/Approach";
-import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
@@ -19,7 +18,6 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        {/* <Clients /> */}
         <Experience />
         <Approach />
         <Footer />
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,10 +18,10 @@ const Footer = () => {
             <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
                 <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Sai Ganesh Tanneru</p>
                 <div className="flex items-center md:gap-3 gap-6">
-                    {socialMedia.map((info) => (
+                    {socialMedia.map((info, index) => (
                         <a target="_blank" href={info.link}>
                             <div
-                                key={info.id}
+                                key={index}
                                 className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
                             >
                                 <img src={info.img} alt="icons" width={20} height={20} />
