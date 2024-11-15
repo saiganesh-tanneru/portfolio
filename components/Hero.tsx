f26058a710b48569ea4c5d022338b1e3bd5d97b3
saiganesh-tanneru <ganesh.sai42@gmail.com>
2024-11-14 20:02:10 -0600

    removed location

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
                 <div className="max-w-[89w] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
                     <TextGenerateEffect className="text-center text-[40px] md:text-5xl lg:text-6xl " words="Transforming Concepts into Seamless User Experiences" />
-                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Sai, a Next.js Developer based in Texas</p>
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Sai, a Next.js Developer</p>
                     <a href="#about">
                         <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
                     </a>
